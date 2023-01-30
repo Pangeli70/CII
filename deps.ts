1c8210e900268999a4d5a71dcc77f8d4c0040373
Paolo Angeli <pangeli70@gmail.com>
2023-01-30 01:02:54 +0100

    Changed deps for deploy

--- deps.ts
+++ deps.ts
@@ -20,15 +20,14 @@ export { CORSService as DrashCorsService } from "https://deno.land/x/drash@v2.5.
 export * as A2D from "https://raw.githubusercontent.com/Pangeli70/apg-2d/master/mod.ts";
 // export * as A2D from "../2D/mod.ts";
 export * as Rst from "https://raw.githubusercontent.com/Pangeli70/apg-rst/master/mod.ts";
-// export * as Svg from "https://raw.githubusercontent.com/Pangeli70/apg-svg/master/mod.ts";
-export * as Svg from "../SVG/mod.ts";
-// export * as Cad from "https://raw.githubusercontent.com/Pangeli70/apg-cad/master/mod.ts";
-export * as Cad from "../CAD/mod.ts";
+export * as Svg from "https://raw.githubusercontent.com/Pangeli70/apg-svg/master/mod.ts";
+//export * as Svg from "../SVG/mod.ts";
+export * as Cad from "https://raw.githubusercontent.com/Pangeli70/apg-cad/master/mod.ts";
+//export * as Cad from "../CAD/mod.ts";
 export * as Uts from "https://raw.githubusercontent.com/Pangeli70/apg-uts/master/mod.ts";
 export * as Lgr from "https://raw.githubusercontent.com/Pangeli70/apg-lgr/master/mod.ts";
 export * as Jsv from "https://raw.githubusercontent.com/Pangeli70/apg-jsv/master/mod.ts";
-// export * as Jsv from "../JSV/mod.ts";
-
+//export * as Jsv from "../JSV/mod.ts";
 export * as Edr from "https://raw.githubusercontent.com/Pangeli70/apg-edr/master/mod.ts";
 export * as Tng from "https://raw.githubusercontent.com/Pangeli70/apg-tng/master/mod.ts";
 //export * as Tng from "../TNG/mod.ts";
